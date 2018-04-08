1ae81aed36183054eaa96b797b365fdf54963d92
Kevin Xu <xuzp2005@21cn.com>
2018-04-08 23:18:49 +0800

    update

---
+++ src/xuzp/maps.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	m := map[string]string {
+		"name": "ccmouse",
+		"course": "golang",
+		"site": "imooc",
+		"quality": "notbad",
+	}
+
+	m2 := make(map[string]int)
+
+	var m3 map[string]int
+	fmt.Println(m, m2, m3)
+
+	fmt.Println("Traversing map")
+
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+
+	fmt.Println("Getting values")
+	courseName, ok := m["course"]
+	fmt.Println(courseName, ok)
+	if causeName, ok := m["ckckk"]; ok {
+		fmt.Println(causeName)
+	} else {
+		fmt.Println("key does not exist")
+	}
+
+	fmt.Println("Deleting values")
+	name, ok := m["name"]
+	fmt.Println(name, ok)
+
+	delete(m, "name")
+	name, ok = m["name"]
+	fmt.Println(name, ok)
+
+
+
+
+}
